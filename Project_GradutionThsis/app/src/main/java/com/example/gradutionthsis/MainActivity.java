78ad4d9675a46b62cdd6d2342b5c8b0577d0083d
Admin <tuongthanh98@gmail.com>
2021-06-03 15:39:30 +0700

    version03

--- Project_GradutionThsis/app/src/main/java/com/example/gradutionthsis/MainActivity.java
+++ Project_GradutionThsis/app/src/main/java/com/example/gradutionthsis/MainActivity.java
@@ -15,33 +15,23 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import com.example.gradutionthsis.activity.AddRelativeActivity;
-import com.example.gradutionthsis.activity.DetailInjectionActivity;
 import com.example.gradutionthsis.activity.VaccineActivity;
-import com.example.gradutionthsis.dto.DetailSchedule;
-import com.example.gradutionthsis.dto.Injection;
 import com.example.gradutionthsis.dto.NotificationTask;
-import com.example.gradutionthsis.dto.Relative;
 import com.example.gradutionthsis.fragments.VaccineFragment;
 import com.example.gradutionthsis.fragments.RegulationFragment;
 import com.example.gradutionthsis.fragments.ProfileFragment;
 import com.example.gradutionthsis.fragments.SettingFragment;
 import com.example.gradutionthsis.service.MyService;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.play.core.internal.m;
-
-import java.util.Calendar;
-import java.util.List;
-
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
+
     private ActionBar toolbar;
     private MenuItem menuItem;
-
     private DBHelper dbHelper;
     private AlarmReceiver alarmReceiver;
-    private NotificationTask notificationTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,12 +51,19 @@ public class MainActivity extends AppCompatActivity {
 
         dbHelper = new DBHelper(this);
 
+//        if (isCheckedObjectNotifyTask())
+//            if (isCancelAlarm()) {
+//                notifyDetailSchedule();
+//            }
+
         if (isCheckedObjectNotifyTask())
             if (isCancelAlarm()) {
-                notifyDetailSchedule();
+                startService();
+            } else {
+                Intent intent = new Intent(getApplicationContext(), MyService.class);
+                getApplicationContext().stopService(intent);
             }
 
-//        startService();
     }
 
     @Override
@@ -76,15 +73,22 @@ public class MainActivity extends AppCompatActivity {
 
     protected void onStart() {
         super.onStart();
+//        if (isCancelAlarm()) {
+//            notifyDetailSchedule();
+//        }
+
         if (isCancelAlarm()) {
-            notifyDetailSchedule();
+            startService();
+        } else {
+            Intent intent = new Intent(getApplicationContext(), MyService.class);
+            getApplicationContext().stopService(intent);
         }
     }
-//
-//    public void startService(){
-//        Intent intent = new Intent(getApplicationContext(), MyService.class);
-//        getApplicationContext().startService(intent);
-//    }
+
+    public void startService() {
+        Intent intent = new Intent(getApplicationContext(), MyService.class);
+        getApplicationContext().startService(intent);
+    }
 
     /**
      * @author: Nguyễn Thanh Tường
@@ -203,128 +207,6 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * @author: Nguyễn Thanh Tường
-     * @date 24/05/2021 : 17h04p
-     */
-    //Thông báo chi tiết lịch tiêm phòng
-    //[START notifyDetailSchedule]
-    private void notifyDetailSchedule() {
-        notificationTask = dbHelper.getAllTask().get(0);
-        String title, message;
-        int day = notificationTask.getDay(); // lấy ngày thông báo
-
-//        List<DetailSchedule> detailSchedules = dbHelper.getDSchedulesByTime(getCurrentDay(day));   //Lấy danh sách có chung ngày thông báo
-
-        List<DetailSchedule> detailSchedules = dbHelper.getDSchedulesByNotify();   //Lấy danh sách trạng thái thông báo của mũi tiêm là 0
-
-        for (DetailSchedule detailSchedule : detailSchedules) {
-            String date = detailSchedule.getInjectionTime().replace('/', '-'); // replace kí tự '/' thành kí tự '-'
-
-            //Kiểm tra trạng thái của chi tiết lịch tiêm phòng
-            //Nếu == 0 thì thông báo
-            if (detailSchedule.getStatus() != DetailInjectionActivity.STATUS_CODE) {
-                detailSchedule.setNotification(1);  //đặt lại notification
-
-                //Kiểm tra giữa ngày tiêm phòng và ngày hiện hành
-                // Ngày hiện hành = ngày hiện hành + day với day là thời gian thông báo.
-                // . Để tiến hành kiểm tra các mũi tiêm đã bỏ lỡ
-                if (DetailInjectionActivity.compareDate(detailSchedule.getInjectionTime(), getCurrentDay(day)) > DetailInjectionActivity.CHECK_STATUS_CODE) {
-
-                    //Cập nhật field notification của chi tiết lịch tiêm phòng trong db
-                    if (dbHelper.updateDetailSchedule(detailSchedule) > 0) {
-                        Relative relative = dbHelper.getRelativeById(detailSchedule.getIdRelative());
-                        Injection injection = dbHelper.getInjectionById(detailSchedule.getIdInjection());
-
-                        if (relative != null) {
-                            title = relative.getFullName();
-                            message = "Đã qua ngày tiêm! \nBạn hãy cập nhật " + injection.getInjectionName() + " của vaccine " + dbHelper.getVaccineById(injection.getIdVaccine()).getVaccination() + "!";
-                            AlarmReceiver.ID_RELATIVE = relative.getIdRelative();
-                            AlarmReceiver.ID_INJECTION = injection.getIdInjection();
-                            isCheckedNotify(setFormat(date), day, notificationTask.getHour(), notificationTask.getMinute(), title, message);
-                        }
-                    }
-                }
-
-                if (DetailInjectionActivity.compareDate(detailSchedule.getInjectionTime(), getCurrentDay(day)) == DetailInjectionActivity.CHECK_STATUS_CODE){
-                    //Cập nhật field notification của chi tiết lịch tiêm phòng trong db
-                    if (dbHelper.updateDetailSchedule(detailSchedule) > 0) {
-                        Relative relative = dbHelper.getRelativeById(detailSchedule.getIdRelative());
-                        Injection injection = dbHelper.getInjectionById(detailSchedule.getIdInjection());
-
-                        if (relative != null) {
-                            title = relative.getFullName();
-                            if (day == 3)
-                                message = "Còn 3 ngày nữa là tới ngày tiêm " + injection.getInjectionName() + " vaccine " + dbHelper.getVaccineById(injection.getIdVaccine()).getVaccination() + " của " + ". \nBạn hãy cập nhật lại thời gian để được báo chính xác nhé!";
-                            else
-                                message = "Đã tới ngày tiêm! \nBạn hãy cập nhật " + injection.getInjectionName() + " của vaccine " + dbHelper.getVaccineById(injection.getIdVaccine()).getVaccination() + "!";
-                            AlarmReceiver.ID_RELATIVE = relative.getIdRelative();
-                            AlarmReceiver.ID_INJECTION = injection.getIdInjection();
-                            isCheckedNotify(setFormat(date), day, notificationTask.getHour(), notificationTask.getMinute(), title, message);
-                        }
-                    }
-                }
-
-            }
-        }
-    }
-    //[END notifyDetailSchedule]
-
-
-    private void notifyMissInjected() {
-
-    }
-
-    /**
-     * @param dayNotify Ngày  trong đối tương NotificationTask
-     * @author: Nguyễn Thanh Tường
-     * @date 24/05/2021 : 16h56p
-     */
-    //Lấy ngày hiện hành
-    //[START getCurrentDay]
-    private String getCurrentDay(int dayNotify) {
-        Calendar c = Calendar.getInstance();
-        c.getTime();//Lấy ngày hiện hành
-        int dayOfMonth = c.get(Calendar.DAY_OF_MONTH) + dayNotify; //cộng ngày thông báo 0 hoặc 3 ngày
-        int month = c.get(Calendar.MONTH);
-        int year = c.get(Calendar.YEAR);
-        return ((dayOfMonth) + "/" + (month + 1) + "/" + year);
-    }
-    //[END getCurrentDay]
-
-
-    /**
-     * @author: Nguyễn Thanh Tường
-     * @date 24/05/2021 16h55p
-     */
-    //Đặt lại định dạng cho ngày thành yyyy-MM-dd
-    //[START setFormat]
-    private String setFormat(String injectionTime) {
-        String[] dateArray = injectionTime.split("-");
-        String dateFormat = (dateArray[2] + "-" + dateArray[1] + "-" + dateArray[0]);
-//        Log.d(TAG, "DateFormat: " + dateFormat);
-        return dateFormat;
-    }
-    //[END setFormat]
-
-
-    /**
-     * @param message nội dung tin
-     * @param date    ngày thông báo định dạng thao yyyy-MM-ddnhắn
-     * @param hour    giờ thông báo
-     * @param minute  phút thông báo
-     * @author: Nguyễn Thanh Tường
-     * @date 18/05/2021 : 9h52p
-     */
-    //Phương thức kiểm tra valid Notify
-    //[START isCheckedNotify]
-    private void isCheckedNotify(String date, int dayNotify, int hour, int minute, String title, String message) {
-        String time = hour + ":" + minute;  // đặt format cho time (thời gian) theo định dạng HH:mm - hour:minute
-        alarmReceiver.setOneAlarmTime(MainActivity.this, AlarmReceiver.TYPE_ONE_TIME, date, dayNotify, time, title, message);
-    }
-    //[END isCheckedNotify]
-
-
-    /**
-     * @author: Nguyễn Thanh Tường
      * @date 18/05/2021 : 9h36p
      */
     //Phương thức kiểm tra xem đã có đối tượng NotificationTask chưa
@@ -333,12 +215,10 @@ public class MainActivity extends AppCompatActivity {
         //Đặt trạng thái thông báo mặc định
         //Kiểm tra xem đã có task trong SQLite chưa
         if (dbHelper.getAllTask().size() > 0) {
-            notificationTask = dbHelper.getAllTask().get(0);    //Lấy task thông báo ra
             return true;
         }
         //Thêm task thông báo mới
         if (dbHelper.insertNotifyTask(new NotificationTask(1, 1, 0, 7, 0)) > 0) {
-            notificationTask = dbHelper.getAllTask().get(0);
             Log.d(TAG, "isCheckedObjectNotifyTask: Tạo thông báo thành công!");
         }
         return false;
--- Project_GradutionThsis/app/src/main/java/com/example/gradutionthsis/service/MyService.java
+++ Project_GradutionThsis/app/src/main/java/com/example/gradutionthsis/service/MyService.java
@@ -7,15 +7,25 @@ import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
+import android.os.Handler;
 import android.os.IBinder;
 import android.util.Log;
 
 import androidx.annotation.Nullable;
 
 import com.example.gradutionthsis.AlarmReceiver;
+import com.example.gradutionthsis.DBHelper;
 import com.example.gradutionthsis.MainActivity;
 import com.example.gradutionthsis.R;
+import com.example.gradutionthsis.activity.DetailInjectionActivity;
+import com.example.gradutionthsis.dto.DetailSchedule;
+import com.example.gradutionthsis.dto.Injection;
+import com.example.gradutionthsis.dto.NotificationTask;
+import com.example.gradutionthsis.dto.Relative;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 
 public class MyService extends Service {
 
@@ -23,6 +33,9 @@ public class MyService extends Service {
     private static final String CHANNEL_DEFAULT_IMPORTANCE = "channel_service";
     private static final String CHANNEL_NAME_SERVICE = "Channel Service";
     private static final int ONGOING_NOTIFICATION_ID = 1;
+
+    private AlarmReceiver alarmReceiver;
+    private DBHelper dbHelper;
 
 
     @Nullable
@@ -33,17 +46,31 @@ public class MyService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-//        String title = intent.getStringExtra("title");
-//        String message = intent.getStringExtra("message");
-//        int idInjection = intent.getExtras().getInt("idInjection");
-//        int notifId = intent.getExtras().getInt("notifId");
-//        AlarmReceiver.showAlarmNotification(this, title, message, notifId, idInjection);
+
+        Log.d(TAG, "onStartCommand: Service is listeninggg!!!");
+
 
 
         startFG();
-        Log.d(TAG, "onStartCommand: Service is listeninggg!!!");
+        Handler mHandler = new Handler();
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //Notification
+                alarmReceiver = new AlarmReceiver();
+                dbHelper = new DBHelper(getApplicationContext());
+
+
+                notifyDetailSchedule();
+                Log.d("LogService", "run: " + new Date().toString());
+                mHandler.postDelayed(this, 1000);
+            }
+        }, 1000);
+
         return START_STICKY;
     }
+
+
 
     @Override
     public void onCreate() {
@@ -86,4 +113,119 @@ public class MyService extends Service {
     //[END startFG]
 
 
+
+    /**
+     * @author: Nguyễn Thanh Tường
+     * @date 24/05/2021 : 17h04p
+     */
+    //Thông báo chi tiết lịch tiêm phòng
+    //[START notifyDetailSchedule]
+    private void notifyDetailSchedule() {
+        NotificationTask notificationTask = dbHelper.getAllTask().get(0);
+        String title, message;
+        int day = notificationTask.getDay(); // lấy ngày thông báo
+
+//        List<DetailSchedule> detailSchedules = dbHelper.getDSchedulesByTime(getCurrentDay(day));   //Lấy danh sách có chung ngày thông báo
+
+        List<DetailSchedule> detailSchedules = dbHelper.getDSchedulesByNotify();   //Lấy danh sách trạng thái thông báo của mũi tiêm là 0
+
+        for (DetailSchedule detailSchedule : detailSchedules) {
+            String date = detailSchedule.getInjectionTime().replace('/', '-'); // replace kí tự '/' thành kí tự '-'
+
+            //Kiểm tra trạng thái của chi tiết lịch tiêm phòng
+            //Nếu == 0 thì thông báo
+            if (detailSchedule.getStatus() != DetailInjectionActivity.STATUS_CODE) {
+                detailSchedule.setNotification(1);  //đặt lại notification
+
+                //Kiểm tra giữa ngày tiêm phòng và ngày hiện hành
+                //Ngày hiện hành = ngày hiện hành + day với day là thời gian thông báo.
+                //Để tiến hành kiểm tra các mũi tiêm đã bỏ lỡ
+                if (DetailInjectionActivity.compareDate(detailSchedule.getInjectionTime(), getCurrentDay(day)) > DetailInjectionActivity.CHECK_STATUS_CODE) {
+
+                    //Cập nhật field notification của chi tiết lịch tiêm phòng trong db
+                    if (dbHelper.updateDetailSchedule(detailSchedule) > 0) {
+                        Relative relative = dbHelper.getRelativeById(detailSchedule.getIdRelative());
+                        Injection injection = dbHelper.getInjectionById(detailSchedule.getIdInjection());
+
+                        if (relative != null) {
+                            title = relative.getFullName();
+                            message = "Đã qua ngày tiêm! \nBạn hãy cập nhật " + injection.getInjectionName() + " của vaccine " + dbHelper.getVaccineById(injection.getIdVaccine()).getVaccination() + "!";
+                            AlarmReceiver.ID_RELATIVE = relative.getIdRelative();
+                            AlarmReceiver.ID_INJECTION = injection.getIdInjection();
+                            isCheckedNotify(setFormat(date), day, notificationTask.getHour(), notificationTask.getMinute(), title, message);
+                        }
+                    }
+                }
+
+                if (DetailInjectionActivity.compareDate(detailSchedule.getInjectionTime(), getCurrentDay(day)) == DetailInjectionActivity.CHECK_STATUS_CODE){
+                    //Cập nhật field notification của chi tiết lịch tiêm phòng trong db
+                    if (dbHelper.updateDetailSchedule(detailSchedule) > 0) {
+                        Relative relative = dbHelper.getRelativeById(detailSchedule.getIdRelative());
+                        Injection injection = dbHelper.getInjectionById(detailSchedule.getIdInjection());
+
+                        if (relative != null) {
+                            title = relative.getFullName();
+                            if (day == 3)
+                                message = "Còn 3 ngày nữa là tới ngày tiêm " + injection.getInjectionName() + " vaccine " + dbHelper.getVaccineById(injection.getIdVaccine()).getVaccination() + " của " + ". \nBạn hãy cập nhật lại thời gian để được báo chính xác nhé!";
+                            else
+                                message = "Đã tới ngày tiêm! \nBạn hãy cập nhật " + injection.getInjectionName() + " của vaccine " + dbHelper.getVaccineById(injection.getIdVaccine()).getVaccination() + "!";
+                            AlarmReceiver.ID_RELATIVE = relative.getIdRelative();
+                            AlarmReceiver.ID_INJECTION = injection.getIdInjection();
+                            isCheckedNotify(setFormat(date), day, notificationTask.getHour(), notificationTask.getMinute(), title, message);
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+    //[END notifyDetailSchedule]
+
+    /**
+     * @param dayNotify Ngày  trong đối tương NotificationTask
+     * @author: Nguyễn Thanh Tường
+     * @date 24/05/2021 : 16h56p
+     */
+    //Lấy ngày hiện hành
+    //[START getCurrentDay]
+    private String getCurrentDay(int dayNotify) {
+        Calendar c = Calendar.getInstance();
+        c.getTime();//Lấy ngày hiện hành
+        int dayOfMonth = c.get(Calendar.DAY_OF_MONTH) + dayNotify; //cộng ngày thông báo 0 hoặc 3 ngày
+        int month = c.get(Calendar.MONTH);
+        int year = c.get(Calendar.YEAR);
+        return ((dayOfMonth) + "/" + (month + 1) + "/" + year);
+    }
+    //[END getCurrentDay]
+
+
+    /**
+     * @author: Nguyễn Thanh Tường
+     * @date 24/05/2021 16h55p
+     */
+    //Đặt lại định dạng cho ngày thành yyyy-MM-dd
+    //[START setFormat]
+    private String setFormat(String injectionTime) {
+        String[] dateArray = injectionTime.split("-");
+        //        Log.d(TAG, "DateFormat: " + dateFormat);
+        return (dateArray[2] + "-" + dateArray[1] + "-" + dateArray[0]);
+    }
+    //[END setFormat]
+
+
+    /**
+     * @param message nội dung tin
+     * @param date    ngày thông báo định dạng thao yyyy-MM-ddnhắn
+     * @param hour    giờ thông báo
+     * @param minute  phút thông báo
+     * @author: Nguyễn Thanh Tường
+     * @date 18/05/2021 : 9h52p
+     */
+    //Phương thức kiểm tra valid Notify
+    //[START isCheckedNotify]
+    private void isCheckedNotify(String date, int dayNotify, int hour, int minute, String title, String message) {
+        String time = hour + ":" + minute;  // đặt format cho time (thời gian) theo định dạng HH:mm - hour:minute
+        alarmReceiver.setOneAlarmTime(this, AlarmReceiver.TYPE_ONE_TIME, date, dayNotify, time, title, message);
+    }
+    //[END isCheckedNotify]
 }
